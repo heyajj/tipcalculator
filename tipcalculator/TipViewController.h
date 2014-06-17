d6017787578ce9425cb87705591f24ae6f0d490d
heyajj <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-06-16 21:43:17 -0700

    Testing 1,2,3...

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Andrew Janich on 6/16/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
