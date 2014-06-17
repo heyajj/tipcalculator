f7839d045c5cca784460c4288b46642691830bd9
Andrew Janich <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-06-16 18:38:11 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Andrew Janich on 6/16/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
